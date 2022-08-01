0019fdebc91b7f58bd2684a6bd7de9d0c21ca3b3
Maryia Kashkan <mary.kashkan@gmail.com>
2022-08-01 03:30:09 +0300

    feat: add a solution of the task

--- src/index.js
+++ src/index.js
@@ -38,8 +38,33 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
-}
+    let r = '';
+    let letter;  
+    let str = '';
+    let space = ' ';
+    let exprNew = expr.match(/.{1,10}/g);    
+    for (let i = 0; i < exprNew.length; i++) {      
+      let exprNewPart = exprNew[i].match(/.{1,2}/g);
+      for (let k = 0; k < exprNewPart.length; k++) {      
+        if (exprNewPart[k] === '10') {
+          r = r + '.';
+        } else if (exprNewPart[k] === '11') {
+          r = r + '-';
+        } else {       
+          r = r;        
+        };     
+        continue;   
+      };
+      letter = MORSE_TABLE[r];
+      if (letter) {
+        str = str + letter;
+      } else {
+        str = str + space;
+      };
+      r = '';
+    };
+    return str;
+  };
 
 module.exports = {
     decode
